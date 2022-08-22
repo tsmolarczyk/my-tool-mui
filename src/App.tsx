996f5eea444d73bdc335e7da689573e80398d326
tsmolarczyk <74697368+tsmolarczyk@users.noreply.github.com>
2022-08-22 15:05:48 +0200

    add tabs with different props, exploration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import NavBar from 'components/Main/NavBar/NavBar'
+import { NavBar } from 'components/Main/NavBar/NavBar'
+import { MyTabs } from 'components/Main/NavBar/Tabs'
 
 function App() {
-  return <NavBar />
+  return (
+    <>
+      <NavBar />
+      <MyTabs />
+    </>
+  )
 }
 
 export default App
--- src/components/Main/NavBar/NavBar.tsx
+++ src/components/Main/NavBar/NavBar.tsx
@@ -5,25 +5,11 @@ import AppBar from '@mui/material/AppBar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
-import Button from '@mui/material/Button'
 
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-
-import { styled, useTheme } from '@mui/material/styles'
+import { useState } from 'react'
 import Drawer from '@mui/material/Drawer'
-import CssBaseline from '@mui/material/CssBaseline'
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
-import Divider from '@mui/material/Divider'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
-import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import MailIcon from '@mui/icons-material/Mail'
+import CloseIcon from '@mui/icons-material/Close'
 
 const darkTheme = createTheme({
   palette: {
@@ -31,65 +17,8 @@ const darkTheme = createTheme({
   },
 })
 
-type Anchor = 'top' | 'left' | 'bottom' | 'right'
-
-export default function NavBar() {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  })
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return
-      }
-
-      setState({ ...state, [anchor]: open })
-    }
-
-  const list = (anchor: Anchor) => (
-    <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  )
-
+export const NavBar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   return (
     <ThemeProvider theme={darkTheme}>
       <AppBar position="static">
@@ -100,6 +29,7 @@ export default function NavBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={() => setIsDrawerOpen(true)}
           >
             <MenuIcon />
           </IconButton>
@@ -110,22 +40,26 @@ export default function NavBar() {
           <Box></Box>
         </Box>
       </AppBar>
-      <Box width="600px">
-        <Drawer
-          anchor="left"
-          open={true}
-          sx={{
-            position: 'absolute',
-            margin: '60px',
-            zIndex: -999,
-            width: '500px',
-          }}
-          onClose={toggleDrawer('left', false)}
-        >
-          {/* {list(anchor)} */}
-          <p>lista</p>
-        </Drawer>
-      </Box>
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+      >
+        <Box p={2} width="250px" textAlign="center">
+          <Typography variant="h6" component="div">
+            MENU
+          </Typography>
+          <Box>lol</Box>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            onClick={() => setIsDrawerOpen(false)}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      </Drawer>
     </ThemeProvider>
   )
 }
