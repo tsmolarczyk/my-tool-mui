b2d758f845d0a516e9899d71207185c9213c2bb5
tsmolarczyk <74697368+tsmolarczyk@users.noreply.github.com>
2022-08-22 13:09:13 +0200

    mui drawer

---
+++ src/components/Main/NavBar/NavBar.tsx
@@ -0,0 +1,131 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+
+import AppBar from '@mui/material/AppBar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Button from '@mui/material/Button'
+
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+import { styled, useTheme } from '@mui/material/styles'
+import Drawer from '@mui/material/Drawer'
+import CssBaseline from '@mui/material/CssBaseline'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right'
+
+export default function NavBar() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  })
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return
+      }
+
+      setState({ ...state, [anchor]: open })
+    }
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position="static">
+        <Box display="flex" justifyContent="space-between" padding="1rem">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant="h4" component="div" sx={{ alignSelf: 'center' }}>
+            Customers
+          </Typography>
+          <Box></Box>
+        </Box>
+      </AppBar>
+      <Box width="600px">
+        <Drawer
+          anchor="left"
+          open={true}
+          sx={{
+            position: 'absolute',
+            margin: '60px',
+            zIndex: -999,
+            width: '500px',
+          }}
+          onClose={toggleDrawer('left', false)}
+        >
+          {/* {list(anchor)} */}
+          <p>lista</p>
+        </Drawer>
+      </Box>
+    </ThemeProvider>
+  )
+}
